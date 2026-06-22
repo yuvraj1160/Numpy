41f6c20331b6a14ef1b99a6fa9a1da5ee877f49a
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-22 23:15:18 +0530

    add example of aggregation functions using numpy arrays

---
+++ Numpy Array Properties/agg_func.py
@@ -0,0 +1,12 @@
+# To perform aggregation functions in array
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print( np.sum(arr))
+print( np.min(arr))
+print( np.std(arr))
+print( np.mean(arr))
+print( np.max(arr))
+print( np.var(arr))
