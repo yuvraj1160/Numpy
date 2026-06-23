36069b799000583ba8974ca6a247ecedfa968cdf
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-23 21:35:24 +0530

    add example to access elements in a numpy array

---
+++ Indexing & Slicing/access.py
@@ -0,0 +1,14 @@
+# To access the element in a array
+
+""" 
+  array[index]  # 1d
+  array[row , coloumn]   # 2d
+"""
+
+import numpy as np
+
+arr = np.array([10 , 20 , 30 , 40 , 50])
+
+print(arr[0])
+print(arr[3])
+print(arr[-1])
