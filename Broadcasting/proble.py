eeae376a54fe47fd33ae56dc4c81bb2dc1c5a992
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-26 01:29:39 +0530

    add example to calculate final prices after discount using loops

---
+++ Broadcasting/proble.py
@@ -0,0 +1,12 @@
+
+prices = [100, 200, 300]
+
+discount = 10  # 10% discount
+
+final_prices = []
+
+for price in prices :
+    final_price = price - (price * discount/ 100)
+    final_prices.append(final_price)
+    
+print(final_prices)
